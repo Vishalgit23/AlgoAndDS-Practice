4a4dfaa18ae52b82bea71a166ada57159ea888ed
Vishalgit23 <vishalmishra4842@gmail.com>
2024-09-11 00:51:52 +0530

    added more patterns in java

---
+++ dsa/src/main/java/com/vishal/dsa/patterns/Pattern5.java
@@ -0,0 +1,13 @@
+package com.vishal.dsa.patterns;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        int row = 5;
+        for(int i=0;i<row;i++){
+            for(int j=row;j>i;j--){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
--- dsa/src/main/java/com/vishal/dsa/patterns/Pattern6.java
+++ dsa/src/main/java/com/vishal/dsa/patterns/Pattern6.java
@@ -1,13 +1,14 @@
 package com.vishal.dsa.patterns;
 
-public class Patterns4 {
+public class Pattern6 {
     public static void main(String[] args) {
         int row =5;
         for(int i=1 ;i<=row;i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i);
+            int k = 1;
+            for (int j = 5; j > i; j--) {
+                System.out.print(k++);
             }
-            System.out.println();
+            System.out.println(i);
         }
     }
 }
